6a4950c95b590193b3da619cb7dea5bfce49421a
Jason Dancocks <jason.dancocks@gmail.com>
2017-08-23 13:19:52 +0100

    sessions continued

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
 	def new
 	end
+
+	def create
+		user = User.find_by(email: params[:sessions][:email].downcase)
+		if user && user.authenticate(params[:sessions][:password])
+			log_in user
+			redirect_to user
+		else
+			redirect dfhdgh
+		end
+
+
+
+		
+	end
+
+	def delete
+		sign_out
+	end
+
 end
